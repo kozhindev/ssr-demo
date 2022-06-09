8ce42cd244903e21381d47dc2dd7e0b806cc21a4
perlexed <perlexed@gmail.com>
2022-06-09 16:42:51 +0700

    init method added

--- src/routes/IndexPage/IndexPage.tsx
+++ src/routes/IndexPage/IndexPage.tsx
@@ -1,22 +1,25 @@
 import * as React from 'react';
 
 import './IndexPage.scss';
-import {useBem, useFetch} from '@steroidsjs/core/hooks';
+import {useBem, useFetch, useSelector} from '@steroidsjs/core/hooks';
 import { useMemo } from 'react';
+import {getUser} from '@steroidsjs/core/reducers/auth';
 
 export default function IndexPage() {
     const bem = useBem('IndexPage');
+
+    const user = useSelector(state => getUser(state));
 
     const {data: receivedMessage, isLoading} = useFetch(useMemo(() => ({
         url: '/api/v1/get-message',
     }), []));
 
-    const greetingMessage = isLoading ? 'загружаем' : receivedMessage;
+    const greetingMessage = isLoading ? 'loading' : receivedMessage;
 
     return (
         <div className={bem.block()}>
             <div>
-                Greeting message: {greetingMessage}
+                Greeting message for user '{user.name}': {greetingMessage}
             </div>
             <img alt='Чюваки' src='/images/wednesdaymydudeswide.jpg' />
         </div>
--- src/shared/Layout/Layout.tsx
+++ src/shared/Layout/Layout.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
-import {useBem} from '@steroidsjs/core/hooks';
+import {useBem, useComponents} from '@steroidsjs/core/hooks';
 import useLayout, {STATUS_OK, STATUS_LOADING} from '@steroidsjs/core/hooks/useLayout';
 
 import {Notifications} from '@steroidsjs/core/ui/layout';
@@ -12,10 +12,10 @@ import {ROUTE_ROOT} from '../../routes';
 export default function Layout(props: React.PropsWithChildren<any>) {
     const bem = useBem('Layout');
 
-    //const components = useComponents();
-    const {status} = useLayout(/*() => components.http.post('/api/v1/init', {
+    const components = useComponents();
+    const {status} = useLayout(() => components.http.post('/api/v1/init', {
         timestamp: Date.now(),
-    })*/);
+    }));
 
     if (status !== STATUS_OK) {
         return status !== STATUS_LOADING ? status : null;
