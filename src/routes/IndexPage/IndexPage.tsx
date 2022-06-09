3e18311f938c1cc893745feffb67cbe1a418e7fd
perlexed <perlexed@gmail.com>
2022-06-09 16:24:05 +0700

    backend request added

--- src/routes/IndexPage/IndexPage.tsx
+++ src/routes/IndexPage/IndexPage.tsx
@@ -1,14 +1,25 @@
 import * as React from 'react';
 
 import './IndexPage.scss';
-import {useBem} from '@steroidsjs/core/hooks';
+import {useBem, useFetch} from '@steroidsjs/core/hooks';
+import { useMemo } from 'react';
 
 export default function IndexPage() {
     const bem = useBem('IndexPage');
 
+    const {data, isLoading} = useFetch(useMemo(() => ({
+        url: '/api/v1/get-message',
+    }), []));
+
+    if (isLoading) {
+        return (<div>
+            Loading...
+        </div>);
+    }
+
     return (
         <div className={bem.block()}>
-            Hello ;)
+            Greeting message: {data}
         </div>
     );
 }
