ee71d27cd18503aa66528a74fa19a5d36dbaadda
perlexed <perlexed@gmail.com>
2022-06-09 18:19:59 +0700

    ssr configured

--- src/shared/Layout/Layout.tsx
+++ src/shared/Layout/Layout.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
-import {useBem, useComponents} from '@steroidsjs/core/hooks';
+import {useBem} from '@steroidsjs/core/hooks';
 import useLayout, {STATUS_OK, STATUS_LOADING} from '@steroidsjs/core/hooks/useLayout';
 
 import {Notifications} from '@steroidsjs/core/ui/layout';
@@ -9,13 +9,14 @@ import Header from '@steroidsjs/core/ui/layout/Header';
 import './Layout.scss';
 import {ROUTE_ROOT} from '../../routes';
 
+export const initAction = (params, dispatch, components) => components.http.post('/api/v1/init', {
+    timestamp: Date.now(),
+});
+
 export default function Layout(props: React.PropsWithChildren<any>) {
     const bem = useBem('Layout');
 
-    const components = useComponents();
-    const {status} = useLayout(() => components.http.post('/api/v1/init', {
-        timestamp: Date.now(),
-    }));
+    const {status} = useLayout(initAction);
 
     if (status !== STATUS_OK) {
         return status !== STATUS_LOADING ? status : null;
--- webpack.js
+++ webpack.js
@@ -1,3 +1,22 @@
+const webpackNodeExternals = require('webpack-node-externals');
+
 require('@steroidsjs/webpack').config({
     port: 9991,
+    // serverPath: using default value - node_modules/@steroidsjs/ssr/index.ts
+    // applicationPath: using default value - src/Application.tsx
+    // initActionPath: using default value - src/shared/Layout/Layout.tsx
+    ssr: {
+        externals: [
+            webpackNodeExternals({
+                allowlist: [/\.(?!(?:tsx?|jsx?|json)$).{1,5}$/i, /^lodash/, /^@steroidsjs/],
+            })
+        ],
+        module: {
+            rules: {
+                ts: {
+                    exclude: /\.\/node_modules\/(?!steroidsjs\/core\/).*/,
+                },
+            },
+        },
+    },
 });
